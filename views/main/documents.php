9787d761d8ead812dddf93633955e87dce05ed56
Igor Kulakov <kulakov.igor@mail.ru>
2017-09-01 10:35:03 +0300

    add page 'Documents'

--- views/main/documents.php
+++ views/main/documents.php
@@ -1,1 +1,21 @@
 <h1>Документы</h1>
+
+<p class="file DOC small_icon">
+    <a href="<?= \yii\helpers\Url::to('/documents/organization_of_insurance_business.doc') ?>">
+        <i class="icon"></i>Закон об организации страхового дела</a>
+</p>
+
+<p class="file RTF small_icon">
+    <a href="<?= \yii\helpers\Url::to('/documents/rules_of_the_bank.rtf') ?>">
+        <i class="icon"></i>Положение Банка России о правилах ОСАГО</a>
+</p>
+
+<p class="file DOC small_icon">
+    <a href="<?= \yii\helpers\Url::to('/documents/tariffs_and_odds.docx') ?>">
+        <i class="icon"></i>Указание банка РОССИИ о тарифах и коэффициентах</a>
+</p>
+
+<p class="file DOC small_icon">
+    <a href="<?= \yii\helpers\Url::to('/documents/law_of_arms.doc') ?>">
+        <i class="icon"></i>Федеральный закон об ОСАГО</a>
+</p>
