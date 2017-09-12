63f697e100fb533ff3e05cbb91bee6d36a85f127
Igor Kulakov <kulakov.igor@mail.ru>
2017-09-12 17:55:11 +0300

    rename column

---
+++ migrations/m170912_145014_rename_column.php
@@ -0,0 +1,16 @@
+<?php
+
+use yii\db\Migration;
+
+class m170912_145014_rename_colomn extends Migration
+{
+    public function safeUp()
+    {
+        return $this->renameColumn('profile', 'id_users', 'user_id');
+    }
+
+    public function safeDown()
+    {
+        return $this->renameColumn('profile', 'user_id', 'id_users');
+    }
+}
